206963b9ad8c4945a208c27542d47b91c2f3cfce
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-03-29 19:25:53 +0300

    adds mongoose connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var configDB = require("./config/database.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -8,6 +10,8 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 
 var router = express.Router();
+
+mongoose.connect(configDB.url);
 
 router.get("/", function(req, res) {
 	res.json({ message: "Hello world" });
