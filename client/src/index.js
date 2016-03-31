6f7b5f574260dc1c73edd0cfda5913d8d8d3aef7
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-03-31 22:56:10 +0300

    adds another mocked event

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,9 @@ var Router = require("react-router");
 
 var MOCK_DATA = [
   {title: "Mocking event", author: "Testing guru", location: "Mockito islands",
-   eventStart: "2016-03-31T17:45:21.021Z", eventEnd: "2016-03-31T20:45:21.021Z", additionalInfo: "No SW designers allowed"}
+   eventStart: "2016-03-31T17:45:21.021Z", eventEnd: "2016-03-31T20:45:21.021Z", additionalInfo: "No SW designers allowed"},
+  {title: "Mysterious event", author: "Anonymous", location: "My basement",
+   eventStart: "2016-03-31T17:45:21.021Z", eventEnd: "2016-03-31T20:45:21.021Z", additionalInfo: "Bring ice cream"}
 
 ];
 
