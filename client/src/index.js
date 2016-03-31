392017bd41aeaf2b6dd62392f94bf92b1cd2cf6d
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-03-31 22:01:10 +0300

    mocks data for table

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
- 
+var Table = require("./table.js");
+var Router = require("react-router");
+
+var MOCK_DATA = [
+  {title: "Mocking event", author: "Testing guru", location: "Mockito islands",
+   eventStart: "2016-03-31T17:45:21.021Z", eventEnd: "2016-03-31T20:45:21.021Z", additionalInfo: "No SW designers allowed"}
+
+];
+
 ReactDOM.render(
-		  <h1>Hello</h1>,
-		    document.getElementById('app')
-	       );
+  <Table data={MOCK_DATA} />,
+  document.getElementById('app')
+);
