8981c04e62f5cd5bea5b184dd0c16441742ab522
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-03-31 22:01:28 +0300

    implements table component

--- client/src/table.js
+++ client/src/table.js
@@ -1,9 +1,29 @@
 var React = require("react");
+var EventRow = require("./event_row.js");
 
 module.exports = React.createClass({
   render: function() {
+
+    var rows = this.props.data.map(function(event) {
+      return <EventRow event={event} key={event.title} />;
+    });
+
     return (
-      <div>Hello from App!! </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Event Title</th>
+            <th>Author</th>
+            <th>Location</th>
+            <th>Starts at</th>
+            <th>Ends at</th>
+            <th>Additional info</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
       );
   }
 });
