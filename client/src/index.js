e07a5ef0fe9b4de65d9a79656997d8a02182aebe
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-04-01 16:22:04 +0300

    adds route for new event & modifies style

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 var ReactDOM = require('react-dom');
 var React = require('react');
 var MainView = require("./main.js");
+var NewEvent = require("./new_event.js");
 var Router = require("react-router").Router;
 var Route = require("react-router").Route;
 var hashHistory = require("react-router").hashHistory;
@@ -8,6 +9,7 @@ var hashHistory = require("react-router").hashHistory;
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={MainView} />
+    <Route path="/new" component={NewEvent} />
   </Router>,
   document.getElementById('app')
 );
