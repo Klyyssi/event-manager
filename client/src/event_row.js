e59dfd1b60dcb99801049e1a987445043fd7adb5
Markus Mulkahainen <markus.v.m.mulkahainen@student.jyu.fi>
2016-04-01 12:05:14 +0300

    formats event start&end dates

--- client/src/event_row.js
+++ client/src/event_row.js
@@ -1,14 +1,16 @@
 var React = require("react");
+var moment = require("moment");
 
 module.exports = React.createClass({
   render: function() {
+    var timeFormat = "DD.MM.YYYY, h:mm:ss";
     return (
       <tr>
         <td>{this.props.event.title}</td>
         <td>{this.props.event.author}</td>
         <td>{this.props.event.location}</td>
-        <td>{this.props.event.eventStart}</td>
-        <td>{this.props.event.eventEnd}</td>
+        <td>{moment(this.props.event.eventStart).format(timeFormat)}</td>
+        <td>{moment(this.props.event.eventEnd).format(timeFormat)}</td>
         <td>{this.props.event.additionalInfo}</td>
       </tr>
     );
